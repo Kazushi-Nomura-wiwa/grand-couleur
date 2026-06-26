8adb20edc7878ef6f64df64c2e9e98dd54b9e626
Kazushi Nomura <kaznom1977@gmail.com>
2026-06-26 13:55:45 +0900

    Refactor code structure for improved readability and maintainability

--- front-page.php
+++ front-page.php
@@ -2,9 +2,11 @@
 <main class="front-page">
     <section class="hero">
         <h2 class="hero__heading">合同会社グランクルール</h2>
+        <h3>一歩ずつ、夢を形に</h3>
+        <h4>あなたの夢を一緒に育てる</h4>
         <p class="hero__text">就労継続支援A型事業所グランクルールは地下鉄東豊線東区役所前4番出口から徒歩一分</p>
         <p class="hero__text">『<strong>IT</strong>』と『<strong>飲食業務</strong>』<br>を中心に行っている事業所です。</p>
-        <a href="/inquiry">お問い合わせ</a>
+        <a href="/inquiry" class="hero__link">お問い合わせ</a>
     </section>
 </main>
 <?php get_footer(); ?>
