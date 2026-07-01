928fe6e9e6a2640f40e38bac19ea8edf62878b11
Kazushi Nomura <kaznom1977@gmail.com>
2026-07-01 14:09:20 +0900

    ヘッダーの背景画像を追加し、ヒーローセクションの構造を修正

--- front-page.php
+++ front-page.php
@@ -1,12 +1,14 @@
 <?php get_header(); ?>
 <main class="front-page">
     <section class="hero">
-        <h2 class="hero__title">合同会社グランクルール</h2>
-        <h3 class="hero__catch">一歩ずつ、夢を形に</h3>
-        <h4 class="hero__lead">あなたの夢を一緒に育てる</h4>
-        <p class="hero__text">就労継続支援A型事業所グランクルールは地下鉄東豊線東区役所前4番出口から徒歩一分</p>
-        <p class="hero__text">『<strong>IT</strong>』と『<strong>飲食業務</strong>』<br>を中心に行っている事業所です。</p>
-        <a href="/inquiry" class="hero__link">お問い合わせ</a>
+        <div class="contaier">
+            <h2 class="hero__title">合同会社グランクルール</h2>
+            <h3 class="hero__catch">一歩ずつ、夢を形に</h3>
+            <h4 class="hero__lead">あなたの夢を一緒に育てる</h4>
+            <p class="hero__text">就労継続支援A型事業所グランクルールは地下鉄東豊線東区役所前4番出口から徒歩一分</p>
+            <p class="hero__text">『<strong>IT</strong>』と『<strong>飲食業務</strong>』<br>を中心に行っている事業所です。</p>
+            <a href="/inquiry" class="hero__link">お問い合わせ</a>
+        </div>
     </section>
     <section class="worries">
         <div class="container">
