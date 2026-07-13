cabf5807988c98ac129bf653211dc34e0884c352
Kazushi Nomura <kaznom1977@gmail.com>
2026-07-13 13:49:14 +0900

    ヘッダーの構造を改善し、サイトヘッダーコンテナを追加して整頓しました

--- header.php
+++ header.php
@@ -10,26 +10,28 @@
 <body <?php body_class(); ?> >
 <?php wp_body_open(); ?>
 <header class="site-header">
-    <h1 class="site-header__logo">
-        <a href="<?php echo esc_url(home_url('/')); ?>" class="site-header__logo-link"><?php bloginfo('name'); ?></a>
-    </h1>
+    <div class="site-header-container">
+        <h1 class="site-header__logo">
+            <a href="<?php echo esc_url(home_url('/')); ?>" class="site-header__logo-link"><?php bloginfo('name'); ?></a>
+        </h1>
 
-    <button class="site-header__toggle" aria-controls="global-nav" aria-expanded="false">
-        <span></span>
-        <span></span>
-        <span></span>
-        <span class="site-header__toggle-text">MENU</span>
-    </button>
+        <button class="site-header__toggle" aria-controls="global-nav" aria-expanded="false">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span class="site-header__toggle-text">MENU</span>
+        </button>
 
-    <nav id="global-nav" class="global-nav">
-        <?php
-            wp_nav_menu([
-                'theme_location' => 'global-nav',
-                'container'     => false,
-                'menu_class'    => 'global-nav__list',
-                'items_wrap'    => '<ul class="%2$s">%3$s</ul>',
-                'fallback_cb'   => false,
-            ]);
-        ?>
-    </nav>
+        <nav id="global-nav" class="global-nav">
+            <?php
+                wp_nav_menu([
+                    'theme_location' => 'global-nav',
+                    'container'     => false,
+                    'menu_class'    => 'global-nav__list',
+                    'items_wrap'    => '<ul class="%2$s">%3$s</ul>',
+                    'fallback_cb'   => false,
+                ]);
+            ?>
+        </nav>
+    </div>
 </header>
