5fb285cf58c98dc94fdb03be5c62f4b9500e6c29
Kazushi Nomura <kaznom1977@gmail.com>
2026-07-15 14:35:21 +0900

    フッターの構造を整理し、スタイルを統一。モバイルとPC用のサブタイトルを統合し、レイアウトを改善。

--- footer.php
+++ footer.php
@@ -1,19 +1,23 @@
     <footer class="site-footer">
         <div class="container">
-            <p class="site-footer__subtitle--pc">安心して一歩踏み出せる場へ。</hp>
-            <h2 class="site-footer__title">合同会社グランクルール</h2>
-            <address class="site-footer__address">
-                〒065-0012<br>
-                札幌市東区北12条東7丁目1-15 セレスタ札幌1F
-            </address>
-            <div class="site-footer__contact">
-                <p class="site-footer__phone">TEL: 011-600-0998</p>
-                <p class="site-footer__email">Mail: <a href="mailto:grancouleur@gmail.com">grancouleur@gmail.com</a></p>
-            </div>
-            <div class="site-footer__inquiry">
-                <a href="https://www.instagram.com/gran.couleur/" class="site-footer__social-link-instagram" target="_blank">インスタグラム</a>
-                <p class="site-footer__subtitle--sp">安心して一歩踏み出せる場へ。</p>
-                <a href="#" class="site-footer__inquiry-link">お問い合わせはこちら</a>
+            <div class="site-footer__main">
+                <div class="site-footer__info">
+                    <p class="site-footer__subtitle site-footer__subtitle--pc">安心して一歩踏み出せる場へ。</p>
+                    <h2 class="site-footer__title">合同会社グランクルール</h2>
+                    <address class="site-footer__address">
+                        〒065-0012<br>
+                        札幌市東区北12条東7丁目1-15 セレスタ札幌1F
+                    </address>
+                    <div class="site-footer__contact">
+                        <p class="site-footer__phone">TEL: <a href="tel:0116000998">011-600-0998</a></p>
+                        <p class="site-footer__email">Mail: <a href="mailto:grancouleur@gmail.com">grancouleur@gmail.com</a></p>
+                    </div>
+                </div>
+                <div class="site-footer__inquiry">
+                    <a href="https://www.instagram.com/gran.couleur/" class="site-footer__social-link-instagram" target="_blank">インスタグラム</a>
+                    <p class="site-footer__subtitle site-footer__subtitle--sp">安心して一歩踏み出せる場へ。</p>
+                    <a href="#" class="site-footer__inquiry-link">お問い合わせはこちら</a>
+                </div>
             </div>
             <hr class="site-footer__divider">
             <nav id="site-footer-nav" class="site-footer-nav">
