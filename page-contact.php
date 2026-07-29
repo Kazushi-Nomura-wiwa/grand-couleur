b18d251c3351af3631113e7fa2f3965172d67758
Kazushi Nomura <kaznom1977@gmail.com>
2026-07-29 14:59:49 +0900

    お問い合わせページの構造を改善し、クラス名を更新。連絡先情報を追加。

--- page-contact.php
+++ page-contact.php
@@ -1,14 +1,29 @@
 <?php get_header(); ?>
 <main class="page-contact">
-    <div class="container">
-        <p class="eyeblow">CONTACT</p>
-        <h2 class="">お問い合わせ</h2>
-        <h3 class="">見学・ご相談を受け付けています、まずはご気軽にご相談ください。<br>以下のフォームに必要事項をご記入の上、送信してください。</h3>
-        <a href="/">よくある質問</a>
-        <?php echo do_shortcode('[contact-form-7 id="64e4be3" title="お問い合わせ"]'); ?>
-        <div class="contact-address">
-
-        </div>   
+    <div class="page-contact__inner container">
+        <p class="page-contact__eyebrow eyeblow">CONTACT</p>
+        <h2 class="page-contact__title">お問い合わせ</h2>
+        <h3 class="page-contact__lead">見学・ご相談を受け付けています、まずはご気軽にご相談ください。<br>以下のフォームに必要事項をご記入の上、送信してください。</h3>
+        <a class="page-contact__faq-link" href="/">よくある質問</a>
+        <div class="page-contact__form">
+            <?php echo do_shortcode('[contact-form-7 id="64e4be3" title="お問い合わせ"]'); ?>
+        </div>
+        <div class="page-contact__address">
+            <h3 class="page-contact__address-title">ご連絡先</h3>
+            <div class="page-contact__business-hours">
+                <h4 class="page-contact__business-hours-title">受付時間</h4>
+                <p class="page-contact__business-hours-text">平日 10:00～17:00</p>
+                <span class="page-contact__business-hours-note">※土日祝日を除く</span>
+            </div>
+            <div class="page-contact__telephone">
+                <h4 class="page-contact__telephone-title">お電話</h4>
+                <a class="page-contact__telephone-link" href="tel:0116000998">011-600-0998</a>
+            </div>
+            <div class="page-contact__email">
+                <h4 class="page-contact__email-title">メール</h4>
+                <a class="page-contact__email-link" href="mailto:grancouleur@gmail.com">grancouleur@gmail.com</a>
+            </div>
+        </div>
     </div>
 </main>
 <?php get_footer(); ?>
