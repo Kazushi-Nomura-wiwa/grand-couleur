272623b37a1b0c1c3e5593d2ed508aa50649bba6
Kazushi Nomura <kaznom1977@gmail.com>
2026-07-29 14:14:54 +0900

    お問い合わせページに説明文とリンクを追加し、連絡先情報のクラス名を更新

--- page-contact.php
+++ page-contact.php
@@ -2,8 +2,11 @@
 <main class="page-contact">
     <div class="container">
         <p class="eyeblow">CONTACT</p>
+        <h2 class="">お問い合わせ</h2>
+        <h3 class="">見学・ご相談を受け付けています、まずはご気軽にご相談ください。<br>以下のフォームに必要事項をご記入の上、送信してください。</h3>
+        <a href="/">よくある質問</a>
         <?php echo do_shortcode('[contact-form-7 id="64e4be3" title="お問い合わせ"]'); ?>
-        <div class="">
+        <div class="contact-address">
 
         </div>   
     </div>
