806b467fd19e5dc7cf05cde8069092c0898e4c08
Kazushi Nomura <kaznom1977@gmail.com>
2026-08-03 14:51:13 +0900

    サンキューページに電話番号リンクとホームリンクを追加し、スタイルを整備

--- page-thanks.php
+++ page-thanks.php
@@ -4,6 +4,9 @@
         <p class="page-thanks__eyebrow eyeblow">CONTACT</p>
         <h2 class="page-thanks__title">お問い合わせを受け付けました</h2>
         <h3 class="page-thanks__lead">お問い合わせいただきありがとうございます。<br>内容を確認後、担当者よりご連絡いたします。</h3>
+        <p class="page-thanks__telephone-text">お急ぎの場合はお電話にてお問い合わせください</p>
+        <a class="page-thanks__telephone-link" href="tel:0116000998">011-600-0998</a>
+        <a class="page-thanks__home-link" href="/">ホームへ戻る</a>
     </div>
 </main>
 <?php get_footer(); ?>
